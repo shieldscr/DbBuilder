dc597fc4b38912424c080189ad60440047f1d7d2
Cory Shields <shieldscr88@gmail.com>
2014-12-18 19:58:03 -0500

    Finished insertSqlBuilder

--- src/main/java/com/shields/dbbuilder/InsertSqlBuilder.java
+++ src/main/java/com/shields/dbbuilder/InsertSqlBuilder.java
@@ -1,19 +1,42 @@
 package com.shields.dbbuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class InsertSqlBuilder {
 
+    String insertSql;
     private int Id;
     private String tableName;
+    private Map<String, String> ValueMap;
 
     public InsertSqlBuilder(String tableName) {
         this.tableName = tableName;
         Id = 0;
+        ValueMap = new HashMap<String, String>();
     }
 
     @Override
     public String toString() {
         Id++;
-        return String.format("INSERT INTO %s (TEST_ID) VALUES ('%s')",tableName , Id);
+        String columns =  "TEST_ID" + ", ";
+        String values = String.format("'%d', ", Id);
+        for (Map.Entry<String, String> entry : ValueMap.entrySet()) {
+            columns += entry.getKey() + ", ";
+            if (entry.getValue().equalsIgnoreCase("null")) {
+                values += entry.getValue() + ", ";
+            } else {
+                values += "'" + entry.getValue() + "', ";
+            }
+        }
+        columns = columns.substring(0, columns.length() - 2);
+        values = values.substring(0, values.length() - 2);
+        insertSql = String.format("INSERT INTO %s (%s) VALUES (%s)", tableName, columns, values).trim();
+        return insertSql;
+    }
+
+    public InsertSqlBuilder set(String columnName, String value) {
+        ValueMap.put(columnName, value);
+        return this;
     }
 }
--- src/test/java/InsertSqlBuilderTest.java
+++ src/test/java/InsertSqlBuilderTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class InsertSqlBuilderTest {
 
@@ -32,6 +33,31 @@ public class InsertSqlBuilderTest {
         String tableName = "MY_TEST_TABLE";
         String expectedSql = "INSERT INTO " + tableName + " (" + TEST_PRIMARY_KEY_ID + ") VALUES ('1')";
         assertEquals(expectedSql, new InsertSqlBuilder(tableName).toString());
+    }
+
+    @Test
+    public void columnNameAndValuePairIsSetByBuilder() {
+        String expectedSql = "INSERT INTO TEST_TABLE (" + TEST_PRIMARY_KEY_ID + ", TEST_COLUMN) VALUES ('1', '123')";
+        assertEquals(expectedSql, builder.set("TEST_COLUMN", "123").toString());
+    }
+
+    @Test
+    public void multipleColumnNameAndValuesAreSetByBuilder() {
+        String expectedSql = "INSERT INTO TEST_TABLE (" + TEST_PRIMARY_KEY_ID + ", TEST_COLUMN, TEST_COLUMN_TWO) VALUES (1, 123, 234)";
+        assertTrue(expectedSql.contains("TEST_COLUMN"));
+        assertTrue(expectedSql.contains("TEST_COLUMN_TWO"));
+        assertTrue(expectedSql.contains("234"));
+    }
+
+    @Test
+    public void addSingleQuotesAroundAllValues() {
+        String expectedSql = "INSERT INTO TEST_TABLE (" + TEST_PRIMARY_KEY_ID + ", foo) VALUES ('1', 'bar')";
+        assertEquals(expectedSql, builder.set("foo", "bar").toString());
+    }
+
+    @Test
+    public void dontAddSingleQuotesIfInputIsNull() {
+        assertTrue(builder.set("foo", "null").toString().contains("null)"));
     }
 
 }
